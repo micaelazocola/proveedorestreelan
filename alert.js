d7f68616af9154033283b54da04f1d271c28ace8
Mica :) <103139051+micaelazocola@users.noreply.github.com>
2023-02-11 17:28:40 -0300

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,26 @@
+
+const btnCrearComp = document.getElementById ('btn-crear-comprobante')
+
+btnCrearComp.addEventListener("click", () => {
+
+    Swal.fire(
+        '¡LISTO!',
+        'Tu comprobante ha sido generado',
+        'success'
+      )
+  
+})
+
+ // MATCHEAR ID DEL BOTON CON ESTA CONSTANTE :DD
+
+const btnGuardarPerfil = document.getElementById ('btn-guardar-perfil')
+
+btnGuardarPerfil.addEventListener("click", () => {
+
+    Swal.fire(
+        '¡LISTO!',
+        'Tu comprobante ha sido generado',
+        'success'
+      )
+  
+})
